7c9bcf9a36b1dca632d3e09994efb4ae1fbd9cbb
Pouul <paulohuang04@gmail.com>
2025-03-17 12:05:27 +0000

    REC1.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,11 @@
 package org.example;
 
+class ler{
+    ler(String entrada){
+
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         try{
@@ -8,31 +14,47 @@ public class Main {
             while(entrada != "exit"){
                 String entrada = scanner.nextLine();
                 entrada = entrada.replaceAll(" ", "");
-                
+                char[] C_entrada = entrada.toCharArray();
+
                 //para ler o entrada por elementos
                 for(int i = 0 ; i < entrada.length() ; i++){
-                char[] C_entrada = entrada.toCharArray();
+                
                 }
                 
                 if(entrada == "REC"){
+                    String[] REC = new String[10];
                     for(i = 0 ; i < 10 ; i++){
                         entrada = scanner.nextLine();
-                    switch(entrada) {
-                        case STOP:
-                          
-                          break;
-                        case PLAY:
-    
-                          break;
-                        case ERASE:
-    
-                          break;
-                        default:
-                          REC(entrada);
-                          }
-                            
+                        switch(entrada) {
+                          case STOP:
+                            int posição = i;
+                            System.out.println("encerrando gravação..." + "(" + posição + "/" + "10)");
+                            break;
+                          case PLAY:
+                            throw new Exception("Comando inválido para gravação");
+                            i--;
+                            break;
+                          case ERASE:
+                            entrada = null;
+                            System.out.println("Gravação apagada");
+                            break;
+                          default:
+                            REC(entrada , i , REC);
+                          }     
                       }
                 }
+
+                if(entrada == "ERASE"){
+                  entrada = null;
+                  System.out.println("Gravação apagada");
+                }
+
+                if(entrada == "PLAY"){
+                  for(i = 0 ; i < posição ; i++){
+                    ler(REC[i]);
+                  }
+                }
+                            
         
     }
 }
--- src/main/java/org/example/REC.java
+++ src/main/java/org/example/REC.java
@@ -1,14 +1,16 @@
 class REC{
-    private String[] REC = new String[10];
+    
 
-    public REC(String entrada) {
-        int posição = 0;
+    public REC(String entrada , int posição , String REC[]) {
+        
         try{
             REC[posição] = entrada;
         }
         catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
         }
-        System.out.println("(" + posição + "/" + "10)");
+        System.out.println("(" + posição + "/" + "10)" + REC[posição]);
     }
+
+    
 }
